4c424ae7e6c638ef05bd1b32d05906a50cb768ba
Frederick Pek <ssixthlhm@gmail.com>
2022-04-20 01:24:57 +0800

    even image widths

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,3 @@
 window.addEventListener('scroll', () => {
   document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
-
-alert("Hi, if you are seeing this alert, its means that "
-	+ "this portfolio is still being updated. "
-	+ "All of the text and figures are only placeholders.");

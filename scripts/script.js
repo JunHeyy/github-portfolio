4683a384ccf1b93ebbcf08bee07b4532f608ad64
Frederick Pek <ssixthlhm@gmail.com>
2022-04-20 17:16:46 +0800

    add google model-viewer module

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,23 @@
+/* navbar rolling cube */
 window.addEventListener('scroll', () => {
   document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 }, false);
+
+/* rubiks cube model */
+const rubiks = document.querySelector('#rubiks');
+rubiks.addEventListener('load', () => {
+	rubiks.play({pingpong: true});
+});
+
+/* reveal on scroll */
+window.addEventListener('scroll', ()=> {
+	let reveals = document.querySelectorAll('.reveal');
+	for (e of reveals) {
+		let windowHeight = window.innerHeight;
+		let revealTop = e.getBoundingClientRect().top;
+		let revealPoint = 120;
+		if (revealTop < windowHeight - revealPoint) {
+			e.classList.add("show");
+		}
+	}
+});
